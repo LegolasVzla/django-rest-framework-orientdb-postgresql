bfc36e1acd5844d936a7378c0701ff38919d9c08
Manuel Carrero <mcomercio.web@gmail.com>
2019-07-06 20:56:16 -0400

    Allowing token in query string

--- core/frontend/api.py
+++ core/frontend/api.py
@@ -11,6 +11,7 @@ from pyorient.ogm.query import Query
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from rest_framework.authentication import TokenAuthentication
 
 User = get_user_model()
 
@@ -19,11 +20,26 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
+class TokenAuthSupportQueryString(TokenAuthentication):
+    """
+    Extend the TokenAuthentication class to support querystring authentication
+    in the form of "http://www.example.com/?auth_token=<token_key>"
+    """
+    def authenticate(self, request):
+        # Check if 'token_auth' is in the request query params.
+        # Give precedence to 'Authorization' header.
+        if 'auth_token' in request.query_params and \
+                        'HTTP_AUTHORIZATION' not in request.META:
+            return self.authenticate_credentials(request.query_params.get('auth_token'))
+        else:
+            return super(TokenAuthSupportQueryString, self).authenticate(request)
+
 class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	permission_classes = (IsAuthenticated,)
 	serializer_class = UserSerializer
 	pagination_class = StandardResultsSetPagination
+	authentication_classes = (TokenAuthSupportQueryString,)
 
 	def destroy(self, request, *args, **kwargs):
 		'''
